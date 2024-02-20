e9e4fe0319f5c38f5686830c3ddc78cf19ac8cda
Gita Shafaq <gitashafaq2613@gmail.com>
2024-02-20 20:50:08 +0430

    Add files via upload

---
+++ java script assignment/app.js
@@ -0,0 +1,242 @@
+
+
+
+
+
+
+
+
+// let numNeigbours =prompt("How many neigbours does your country?");
+// if (numNeigbours==1){
+//     console.log("Only one border");
+// }else if(numNeigbours >1){
+//     console.log("more than one border");
+// }else if(numNeigbours==1 || numNeigbours<1){
+//     console.log("No border");
+// }
+
+
+
+
+
+
+// if (numNeigbours===1){
+//     console.log("Only one border");
+// }else if(numNeigbours >1){
+//     console.log("more than one border");
+// }else if(numNeigbours=== 0 || numNeigbours< -1){
+//     console.log("No border");
+// }
+
+
+
+
+// let numNeigbours =prompt("How many neigbours does your country?");
+// if (numNeigbours==1){
+//     console.log("Only one border");
+// }else if(numNeigbours >1){
+//     console.log("more than one border");
+// }else if(numNeigbours==1 || numNeigbours<1){
+//     console.log("No border");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Logical operator
+
+
+// let country = "Portaghal";
+// let language = "English";
+// let population = 50;
+
+
+
+//  if(country=="Portaghal" && language =="English" && population =="50"){
+//     console.log("she can life here.");
+
+//  }else{
+//     console.log("she can not life here");
+//  }
+
+
+
+
+
+
+
+
+// switch case
+  
+// let language = "amrica";
+// switch(language)
+// {
+//     case "chaines":console.log("Most Number of native speakers");
+//     break;
+
+//     case "spanish":console.log("2nd place in number of native speaker");
+//     break;
+
+//     case "english":console.log("3rd place");
+//     break;
+
+//     case "hindi":console.log("Number 4");
+//     break;
+     
+//     case "arabic":console.log("5th most spoken language");
+//     break;
+
+//     default:console.log("Great language to D");
+
+// }
+
+
+
+
+
+
+//function
+
+// function describeCountry(country,population,capital){
+   
+//     let result=country  + "has"+population  + "people and its capital city is"  + capital
+   
+//     return result;
+// }
+
+// let finland=describeCountry(" Finlad ", " 6 Milion ", " Helsinki ");
+// let afghanistan= describeCountry(" Afghanistan ", " 30 Milion ", " Kabul ");
+// let amrica=describeCountry(" Amrica", " 22 Milion ", " Washington");
+//     console.log(afghanistan);
+
+
+     
+
+
+
+
+// function decliration vs expressions
+
+
+//    function percentageQfWorld1(population){
+//     let result= population;
+//     return result;
+//    }
+//    let result=percentageQfWorld1(1441);
+//     let avg =result/7900*100;
+//     console.log(avg +"%");
+    
+    
+
+
+
+// introductio to array
+
+// let population=[74646,54646,6666,64667];
+// console.log(population);
+
+
+
+
+
+function percentageQfWorld1(percentages){
+        let result= percentages;
+        return result;
+       }
+       let result=percentageQfWorld1[144466666];
+        let avg =result/7900*100;
+        console.log(avg +"%");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Basic array operation and metods
+
+// let neighbours=["Sweden","Amrica","Afghanistan"];
+// neighbours.push("Armashair");
+// console.log(neighbours);
+
+
+
+
+
+
+
+// let neighbours=["Sweden","Amrica","Afghanistan","Armashair"];
+// delete neighbours[3];
+// console.log(neighbours);
+
+
+
+
+
+// let neighbours=["Sweden","Amrica","Afghanistan"];
+// let result=neighbours.indexOf("Sweden");
+// console.log(result);
+
+
+
+
+// let neighbours=["Sweden","Amrica","Afghanistan"];
+//   neighbours[0]="Repablic of swedan";
+// console.log(neighbours);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
